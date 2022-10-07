6f6f3349dd381fc3b27543cd5dd8361a187e75a1
stiztics <il2149@nyu.edu>
2022-10-07 04:59:27 -0400

    Fixed acceleration speed of player

--- Assets/Code/PlayerCode.cs
+++ Assets/Code/PlayerCode.cs
@@ -12,10 +12,10 @@ public class PlayerCode : MonoBehaviour
 
     public string main_lv_name = "MainStage";
     public string gap_lv_name = "GapRoom";
-    public string wall_lv_name = "";
+    public string wall_lv_name = "ClosingWalls";
     public string maze_lv_name = "FallingMaze";
     public string monster_lv_name = "MonsterRoom";
-    public string exit_lv_name = "";
+    public string exit_lv_name = "FinalRoom";
     
     // Start is called before the first frame update
     void Start()
@@ -24,6 +24,7 @@ public class PlayerCode : MonoBehaviour
         mainCam = Camera.main;
         if(PublicVars.items["boots"]){
             _agent.speed = 15f;
+            _agent.acceleration = 60f;
         }
     }
 
@@ -64,6 +65,8 @@ public class PlayerCode : MonoBehaviour
         }
         if (other.CompareTag("ToWallClose"))
         {
+            print("HITTTT");
+            print(wall_lv_name);
             SceneManager.LoadScene(wall_lv_name);
         }
         if (other.CompareTag("ToMaze"))
@@ -72,7 +75,9 @@ public class PlayerCode : MonoBehaviour
         }
         if (other.CompareTag("ToExit"))
         {
-            SceneManager.LoadScene(exit_lv_name);
+            if(PublicVars.items["key"]){
+                SceneManager.LoadScene(exit_lv_name);
+            }
         }
         if(other.CompareTag("Gap")){
             Rigidbody player = GetComponent<Rigidbody>();
