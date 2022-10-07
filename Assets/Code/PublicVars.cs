e3550e4ddbe88398ad61bb05a8175ef6ecc03c5f
stiztics <il2149@nyu.edu>
2022-10-07 07:06:42 -0400

    Added more monsters and made them faster

--- Assets/Code/PublicVars.cs
+++ Assets/Code/PublicVars.cs
@@ -7,9 +7,9 @@ public static class PublicVars
     public static Dictionary<string, bool> items = new Dictionary <string, bool>
     {
         {"goggles", false},
-        {"boots", false},
+        {"boots", true},
         {"chalice", false},
-        {"bow", false},
+        {"bow", true},
         {"key", false},
         {"bridge", false}
     };
