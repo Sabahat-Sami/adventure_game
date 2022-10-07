7ab9ecf6be1a78edad12966221e03285c8dfa225
stiztics <il2149@nyu.edu>
2022-10-07 06:54:18 -0400

    Fixed new game reset and music volume

--- Assets/Code/EndScreen.cs
+++ Assets/Code/EndScreen.cs
@@ -7,6 +7,13 @@ using UnityEngine.SceneManagement;
 public class EndScreen : MonoBehaviour
 {
     public void BackToLobby(){
+        PublicVars.items["goggles"] = false;
+        PublicVars.items["boots"] = false;
+        PublicVars.items["chalice"] = false;
+        PublicVars.items["bow"] = false;
+        PublicVars.items["key"] = false;
+        PublicVars.items["bridge"] = false;
+        PublicVars.playerHealth = 3;
         SceneManager.LoadScene("StartMenu");
     }
 
